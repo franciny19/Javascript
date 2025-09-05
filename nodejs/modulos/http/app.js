01d714704ea2efa8f821bfc48bd20881bb250f1d
Franciny <francinybr2020@gmail.com>
2025-09-05 09:24:58 -0300

    http

--- nodejs/modulos/http/app.js
+++ nodejs/modulos/http/app.js
@@ -1,11 +1,12 @@
 //Importando modulo http
-const http = requise ('http');
+const http = require('http');
 
 //criando um servidor que responde com "Hello Wolrd"
 http.createServer((req, res) => {
     //Definindo o cabecalho da resposta com status 200 (OK) e tipo de conteudo text/plain
     res.writeHead(200, {'Content-Type': 'text/plain'});
     //enviando a resposta "Hello World!"
-    res.end('Hello World!');
+    res.end('Ola, Mundo!');
     //adicinonando um log no console para cada requisicao recebida
-}).listen(8080)
+}).listen(8080);
+console.log('Servidor rodando em http://localhost:8080/');
